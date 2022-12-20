e56a6c22613271dce2b0b6ce9cbacc47c565bfba
Tish Faroul <lfaroul14@gmail.com>
2022-12-19 23:48:22 -0500

    beginning project

--- designs.js
+++ designs.js
@@ -1,7 +1,19 @@
 // Select color input
-// Select size input
 
-// When size is submitted by the user, call makeGrid()
+const pickColor = document.getElementById('colorPicker');
+const pickSize = document.getElementById('sizePicker');
+
+pickSize.addEventListener('submit', ((e) => {
+    e.preventDefault();
+
+    while(table.hasChildNodes()){
+        table.removeChild(table.firstChild)
+    }
+}));
+
+
+const table = document.querySelector('pixelCanvas');
+
 
 function makeGrid() {
 
